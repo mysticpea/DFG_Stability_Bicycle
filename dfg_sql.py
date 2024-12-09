ee9c7d57628426e27727156ecf616b91ddac0a4a
roee-zehavi <roee.zehavi@gmail.com>
2024-12-09 16:02:24 +0200

    טבלת היסטוריה

--- dfg_sql.py
+++ dfg_sql.py
@@ -9,11 +9,12 @@ db = mysql.connector.connect(
 
 mycursor = db.cursor()
 
-# הכנסת מידע על משתמש
-insert_user_query = """
-INSERT INTO users (user_id, name, gender, age)
-VALUES (%s, %s, %s, %s)
-"""
+# # הכנסת מידע על משתמש
+# insert_user_query = """
+# INSERT INTO users (user_id, name, gender, age)
+# VALUES (%s, %s, %s, %s
+# )
+# """
 user_data = (209146216, 'roee zehavi', 'm', 26)
 
 # יצירת טבלת משתמש
@@ -54,8 +55,15 @@ CREATE TABLE exercise_history (
     exercise_ID INT,
     score INT,
     date DATE,
+    Direction ENUM('f', 'b', 'l', 'r', 'h'),
+    angel INT,
+    time INT,
+    response_angel INT,
+    reaction_time INT,
+    quality INT,
+    success_rate INT,
     FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (exercise_ID) REFERENCES exercise_progrems(exercise_ID)
+    FOREIGN KEY (exercise_ID) REFERENCES exercise_progrems(exercise_ID),
 )
 """
 
@@ -73,9 +81,6 @@ CREATE TABLE exercise_sement (
     FOREIGN KEY (exercise_ID) REFERENCES exercise_progrems(exercise_ID)
 )
 """
-#הוספת עומודה בטבלה שנקראת For_test
-alter_table_query = f"ALTER TABLE {'exercise_sement'} ADD COLUMN {'For_test'} {'INT'}"
-
 # הכנסת נתונים לטבלת סגמנטים
 
 insert_exercise_sement_query = """
@@ -125,6 +130,6 @@ alter_table_query8 = f"SELECT * FROM {'exercise_sement'} WHERE {'exercise_ID'} =
 #             בחירת ביצוע פעולה- לבחור פעולה ואת להוריד את הערה בשורה למטה
 
 
-mycursor.execute(create_exercise_sement_table_query)
+mycursor.execute(create_exercise_history_table_query)
 # mycursor.execute(insert_exercise_sement_query, sement_data)
 db.commit()
